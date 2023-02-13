4681b6a5f81cdf5236b33205d296a824bfc1a6da
Susana Serrano <92804411+KGenesi@users.noreply.github.com>
2023-02-12 23:47:35 -0600

    Update streamlit_app.py
    added new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.subtitle('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal")
+streamlit.text('Kate, Spi nach & Rocket Smoothie")
+streamlit.text('Hard—Boi led Free—Range Egg")
